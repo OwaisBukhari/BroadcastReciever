422bf10e80a8d6259a4ebb2871a24153d558d6bd
OwaisBukhari <owaisbukhari257@gmail.com>
2022-01-13 12:26:33 +0500

    Show toast msg recieved

---
+++ app/src/main/java/com/mafaz/Broadcast/MainActivity.java
@@ -0,0 +1,24 @@
+package com.mafaz.Broadcast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    // broadcast a custom intent.
+
+    public void broadcastIntent(View view){
+        Intent intent = new Intent();
+        intent.setAction("com.tutorialspoint.CUSTOM_INTENT"); sendBroadcast(intent);
+    }
+}
---
+++ app/src/main/java/com/mafaz/Broadcast/MyReceiver.java
@@ -0,0 +1,14 @@
+package com.mafaz.Broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Intent Detected."+Thread.currentThread().getId(), Toast.LENGTH_LONG).show();
+
+    }
+}
